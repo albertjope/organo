25fd907c4e4d2fd203a68a463953ed1a659ab52d
albert.pereira <albert.pereira@mobizapsp.com.br>
2024-04-14 23:19:29 -0300

    Projeto final do curso alura

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import "./Banner.css"
+function Banner(){
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Imagem de banner do Organo" />
+        </header>
+        
+    )
+}
+
+export default Banner;
